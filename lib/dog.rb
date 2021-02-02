b30f3a1555ddfdb38f06f373dc4cda57a473add6
Tammarra McPhaul <shaemc22@gmail.com>
2021-02-02 06:19:17 -0500

    commit tree 5776ccc5079dc98bb4f87b847710373d0725cb7a

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,52 @@
-# Add your code here
+class Dog
+
+attr_accessor :name    
+@@all = []
+
+
+
+    def initialize(name)
+    @name = name
+    save
+    end
+
+def self.all
+    @@all
+end
+
+def self.print_all
+    @@all.each do |doggy|
+        puts doggy.name
+    end
+end
+
+def self.clear_all
+    @@all.clear
+
+end
+
+def save
+    @@all << self
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
